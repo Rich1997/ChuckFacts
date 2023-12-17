efe50ff4870f0198ef0f0ff449360e65704c3a61
Rich1997 <rudronil1997@gmail.com>
2023-12-17 16:23:25 +0530

    Add custom font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,10 +2,12 @@ const Hero = () => {
     return (
         <section className="relative overflow-hidden py-24 md:py-36 lg:py-48 w-full h-2/3 bg-gradient-to-tr from-[#e9a17b] to-[#d98255] text-white">
             <div className="p-16 md:p-24 lg:p-36">
-                <div className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-black">
+                <div className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-black font-KlipanBlack">
                     ChuckFacts
                 </div>
-                <div></div>
+                <div className="text-lg sm:text-xl md:text-2xl lg:text-3xl leading-tight py-2 sm:py-3 md:py-4 lg:py-6">
+                    Repository of Chuck Norris awesomeness
+                </div>
             </div>
         </section>
     );
