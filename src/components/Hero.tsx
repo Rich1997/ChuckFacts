abd1b2411f74085426cd26071f1512803ed82235
Rich1997 <rudronil1997@gmail.com>
2023-12-17 13:56:05 +0530

    Initial commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+    return (
+        <section className="relative overflow-hidden py-24 md:py-36 lg:py-48 w-full h-2/3 bg-gradient-to-tr from-[#e9a17b] to-[#d98255] text-white">
+            <div className="p-16 md:p-24 lg:p-36">
+                <div className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-black">
+                    ChuckFacts
+                </div>
+                <div></div>
+            </div>
+        </section>
+    );
+};
+export default Hero;
